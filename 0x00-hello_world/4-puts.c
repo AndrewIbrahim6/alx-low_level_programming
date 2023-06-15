12a65b8f8dc99e96a893a77ccf96ceec73b9bc21
AndrewIbrahim6 <andrewgalaxy6@gmail.com>
2023-06-15 08:59:37 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
+/**This program prints a message to the console. **/ 
 #include <stdio.h>
-int main() {
-  puts("\"Programming is like building a multilingual puzzle\n");
-  return 0;
+int main(void) {
+// Print the message
+puts("\"Programming is like building a multilingual puzzle");
+// Return 0 to indicate successful execution
+return 0;
 }
