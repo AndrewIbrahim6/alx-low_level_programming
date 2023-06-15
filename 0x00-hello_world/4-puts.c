2faa9bd4f3b91fedb2213c8ee020d12737343363
AndrewIbrahim6 <andrewgalaxy6@gmail.com>
2023-06-15 09:37:23 -0700

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 /*
  * This function is the entry point of the program and prints
  * a message to the console
- * */
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
